121f3ce780a1e730254e7515e16e5802fe43aeb3
Осенчаков Роман <rongo@yandex.ru>
2025-08-27 22:54:27 +0300

    Fix work with env var

--- main.go
+++ main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 	server "github.com/karabasBaRaBaS-1276/go-basic-final/pkg/server"
 )
 
+// Загрузка переменных окружения
+// Принимает на вход указатель на логгер
+func loadEnv(log *log.Logger) {
+	if err := godotenv.Load(); err != nil {
+		// Если файла нет - это нормально для production/CI
+		if !os.IsNotExist(err) {
+			// Прекращаем работу если иные проблемы с файлом
+			log.Fatal("Ошибка загрузки .env файла:", err)
+		}
+	}
+	// Установка переменных по умолчанию
+	setDefaultEnv("TODO_PORT", "7540") // Порт для запуска веб сервера по умолчанию
+}
+
+// Установка окружений по умолчанию, если их нет
+func setDefaultEnv(key, defaultValue string) {
+	if os.Getenv(key) == "" {
+		log.Printf("Умолчание для %s = %s", key, defaultValue)
+		os.Setenv(key, defaultValue)
+	}
+}
+
 func main() {
 	log := log.Default()
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Ошибка загрузки .env файла")
-	}
+	loadEnv(log)
+
 	webServer := server.Get(log)
 
 	log.Println("Запускаем веб сервер")
